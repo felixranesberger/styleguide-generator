475eac20f7eb08767edb36db159925951baad092
Felix Ranesberger <felix.ranesberger@in2code.de>
2025-01-10 21:27:19 +0100

    chore(feat): setup local vite testing server

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
+import fs from 'fs-extra'
+import { createServer } from 'vite'
 import { buildStyleguide } from './lib';
 
 (async () => {
+  // clear the output directory
+  await fs.remove('./styleguide-export')
+
   await buildStyleguide({
     mode: 'production',
     outDir: './styleguide-export',
     contentDir: './test/',
-    projectTitle: 'Universität zu Köln',
+    projectTitle: 'Project name',
     html: {
       lang: 'de',
       assets: {
@@ -22,4 +27,15 @@ import { buildStyleguide } from './lib';
       },
     },
   })
+
+  const server = await createServer({
+    root: './styleguide-export',
+    server: {
+      port: 3000,
+      host: true,
+    },
+  })
+  await server.listen()
+  server.printUrls()
+  server.bindCLIShortcuts({ print: true })
 })()
