5327848d5e6ffdad555a38a67d8a5005f505de48
Felix Ranesberger <felix.ranesberger@in2code.de>
2025-01-29 09:32:12 +0100

    chore: build dist files

--- dist/node/lib/index.mjs
+++ dist/node/lib/index.mjs
@@ -702,7 +702,7 @@ function getMainContentIcons(section) {
             class="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4"
         >
             ${section.icons?.map((icon) => `
-              <li class="relative grid gap-4 rounded-2xl border border-transparent px-4 py-6 transition duration-700 items icon-search-list__item bg-styleguide-bg-highlight hover:border-styleguide-border focus:border-styleguide-border">
+              <li class="relative grid gap-4 rounded-2xl border border-transparent px-4 py-6 transition-[border] duration-700 items icon-search-list__item bg-styleguide-bg-highlight hover:border-styleguide-border focus:border-styleguide-border">
                   <div class="relative flex w-full justify-center size-6 text-styleguide-highlight [&>svg]:h-full [&>svg]:w-auto">
                       ${icon.svg}
   
