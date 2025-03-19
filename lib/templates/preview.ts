114cd91db588754ef45150b5df4013567ac979b6
Felix Ranesberger <felix.ranesberger@in2code.de>
2025-03-19 14:21:58 +0100

    fix(feat): don't make summary text selectable

--- lib/templates/preview.ts
+++ lib/templates/preview.ts
@@ -325,7 +325,7 @@ function getMainContentRegular(section: in2Section, config: StyleguideConfigurat
         <!-- Code -->
         <details class="group">
            <summary class="flex cursor-pointer justify-between items-center rounded-b-2xl px-6 text-sm font-light bg-styleguide-bg">
-                <span class="flex gap-2 items-center py-4">
+                <span class="flex gap-2 items-center py-4 select-none">
                     <svg class="h-4 w-4 group-open:rotate-90 transition-transform" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
                         <path fill-rule="evenodd" d="M6.22 4.22a.75.75 0 0 1 1.06 0l3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06L8.94 8 6.22 5.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd"/>
                     </svg>
